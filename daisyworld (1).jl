d75f341c80b9275ad60862cba2e13e84dd613459
Djamal Avdadaev <146478987+dgavdadaev@users.noreply.github.com>
2026-06-11 11:46:55 +0300

    Update daisyworld (1).jl

--- daisyworld (1).jl
+++ daisyworld (1).jl
@@ -12,10 +12,11 @@ black(a) = a.breed == :black
 white(a) = a.breed == :white
 adata = [(black, count), (white, count)]
 
+## Параметры эксперимента
 param_dict = Dict(
     :griddims => (30, 30),
-    :max_age => [25, 40],
-    :init_white => [0.2, 0.8],
+    :max_age => [25, 40], 
+    :init_white => [0.2, 0.8], 
     :init_black => 0.2,
     :albedo_white => 0.75,
     :albedo_black => 0.25,
@@ -26,6 +27,7 @@ param_dict = Dict(
     :seed => 165,
 )
 
+## Создаём список всех комбинаций
 params_list = dict_list(param_dict)
 
 for params in params_list
@@ -44,7 +46,7 @@ for params in params_list
     figure[1, 2] = Legend(figure, [blackl, whitel], ["black", "white"])
 
     ax2 = figure[2, 1] = Axis(figure, ylabel = "temperature") # Показатели параметров
-    ax3 = figure[3, 1] = Axis(figure, xlabel = "tick", ylabel = "luminosity")
+    ax3 = figure[3, 1] = Axis(figure, xlabel = "tick", ylabel = "luminosity") 
     lines!(ax2, model_df[!, :time], model_df[!, :temperature], color = :red)
     lines!(ax3, model_df[!, :time], model_df[!, :solar_luminosity], color = :red)
     for ax in (ax1, ax2); ax.xticklabelsvisible = false; end
